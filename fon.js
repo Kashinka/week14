1f02e6c3c9301c1085a8408a4b858c36c17a0c2c
Kashinka <azalena@yandex.ru>
2022-10-16 23:30:56 +0300

    Added functions

---
+++ fon.js
@@ -0,0 +1,23 @@
+function changeColor1 () {
+
+    let button1 = document.getElementById('button1'); 
+
+    button1.style.background = 'red';
+
+}
+
+function changeColor2 () {
+
+    let button1 = document.getElementById('button2'); 
+
+    button1.style.background = 'red';
+
+}
+
+function changeColor3 () {
+
+    let button1 = document.getElementById('button3'); 
+
+    button1.style.background = 'red';
+
+}
