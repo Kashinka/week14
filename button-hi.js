d358503cfc8761e755e6df4e0e9e229a2231140e
Kashinka <azalena@yandex.ru>
2022-10-16 22:17:59 +0300

    Changed button with using input

--- button-hi.js
+++ button-hi.js
@@ -1,3 +1,13 @@
+/*Перепишите домашнее задание прошлой недели c помощью input для ввода данных и div (либо input) для вывода результата */
+
+function showMessage() {
+    let input = document.getElementById('name');
+
+	console.log(input.value);
+
+    document.getElementById('hi').innerHTML = 'Привет, '+ input.value;
+}
+
 /* Создайте страницу, которая по нажатию на кнопку спрашивает имя у пользователя и выводит персонифицированное приветственное сообщение типа "Привет, Маша!". */
 
 /*function showMessage() {
@@ -9,10 +19,10 @@ alert('Привет, ' + name);
 /*let showMessage = prompt('Как тебя зовут?', "Имя") => alert("Привет!");
 showMessage();*/
 
-let showMessage = () => {
+/*let showMessage = () => {
 
     let name = prompt('Как тебя зовут?', "Имя");
 
     alert('Привет, ' + name);
 
-};
+};*/
